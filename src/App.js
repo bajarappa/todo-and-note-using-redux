c4a13731a14711c03047b5a656613d5b8a1eae46
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-10-27 20:53:06 +0530

    created routes for the app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home";
 import TodoForm from "./components/ToDoForm/ToDoForm";
 import TodoList from "./components/ToDoList/ToDoList";
-import { store } from "./redux/store";
-import "./App.css";
-import { Provider } from "react-redux";
+import NoteForm from "./components/NoteForm/NoteForm";
+import NavBar from "./components/NavBar/NavBar";
+import NoteList from "./components/NoteList/NoteList";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const createTodo = (text) => {
-    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
-  };
-
-  const toggleTodo = (index) => {
-    const list = [...todos];
-    list[index].completed = !list[index].completed;
-    setTodos(list);
-  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "todo",
+      element: (
+        <div>
+          <NavBar />
+          <h1>To Dos</h1>
+          <TodoForm />
+          <TodoList />
+        </div>
+      ),
+    },
+    {
+      path: "notes",
+      element: (
+        <div>
+          <NavBar />
+          <h1>Notes</h1>
+          <NoteForm />
+          <NoteList />
+        </div>
+      ),
+    },
+  ]);
 
   return (
     <div>
-      <h1>To Do App</h1>
       <Provider store={store}>
-        <TodoForm onCreateTodo={createTodo} />
-        <TodoList todos={todos} onToggle={toggleTodo} />
+        <RouterProvider router={router} />
       </Provider>
     </div>
   );
