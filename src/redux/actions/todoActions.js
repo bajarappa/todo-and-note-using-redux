543d53690939c22ef71514a9e3a39746b4d7eecf
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-10-27 10:16:03 +0530

    created redux components

---
+++ src/redux/actions/todoActions.js
@@ -0,0 +1,7 @@
+// Action constants
+export const ADD_TODO = "Add Todo";
+export const TOGGLE_TODO = "Toggle Todo";
+
+// Action creators
+export const addTodo = (text) => ({ text, type: ADD_TODO });
+export const toggleTodo = (index) => ({ index, type: TOGGLE_TODO });
---
+++ src/redux/store.js
@@ -0,0 +1,4 @@
+import redux from "redux";
+import { todoReducer } from "./reducers/todoReducer";
+
+export const store = redux.createStore(todoReducer);
